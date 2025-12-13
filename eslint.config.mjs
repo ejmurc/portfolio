6ada4110f918a97ee7c5418a803bd9ce9c74d10e
Elias Murcray <ejmurc@Mac.attlocal.net>
2025-12-13 10:11:36 -0800

    Precise wording

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,17 +3,17 @@ import tseslint from "typescript-eslint";
 import globals from "globals";
 
 export default tseslint.config(
-	eslint.configs.recommended,
-	...tseslint.configs.recommended,
-	{
-		ignores: ["**/public/*", "**/node_modules/*"],
-	},
-	{
-		files: ["**/*.{js,cjs,mjs,ts}"],
-		languageOptions: {
-			globals: {
-				...globals.browser,
-			},
-		},
-	},
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ["**/public/*", "**/node_modules/*"],
+  },
+  {
+    files: ["**/*.{js,cjs,mjs,ts}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
 );
--- webpack.prod.mjs
+++ webpack.prod.mjs
@@ -7,36 +7,36 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-	mode: "production",
-	entry: "./src/app.ts",
-	output: {
-		path: path.resolve(__dirname, "public"),
-		filename: "bundle.js",
-		clean: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				use: "ts-loader",
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.css$/,
-				use: [MiniCssExtractPlugin.loader, "css-loader"],
-			},
-		],
-	},
-	resolve: {
-		extensions: [".ts", ".js"],
-	},
-	plugins: [
-		new HTMLWebpackPlugin({
-			template: "./src/index.html",
-			minify: true,
-		}),
-		new MiniCssExtractPlugin({
-			filename: "styles.css",
-		}),
-	],
+  mode: "production",
+  entry: "./src/app.ts",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+      minify: true,
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+  ],
 };
