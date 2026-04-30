660249689a0f37fd6ef6a8e41f00c6845d025d46
ejmurc <eliasmurcray@gmail.com>
2026-04-30 04:45:42 -0700

    Add .prettierrc, esbuild, update html content

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,19 @@
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import globals from "globals";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    ignores: ["**/public/*", "**/node_modules/*"],
-  },
-  {
-    files: ["**/*.{js,cjs,mjs,ts}"],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        ignores: ['**/public/*', '**/node_modules/*'],
     },
-  },
+    {
+        files: ['**/*.{js,cjs,mjs,ts}'],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+        },
+    }
 );
